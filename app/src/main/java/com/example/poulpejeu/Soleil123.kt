ddfb4f6f368756906b5ae0204253c5555d77c645
BalusCodeur <nathan.basle@univ-rennes1.fr>
2023-04-10 14:55:39 +0200

    2eme v faut rajouter les bruitages et cleaner

--- app/src/main/java/com/example/poulpejeu/Soleil123.kt
+++ app/src/main/java/com/example/poulpejeu/Soleil123.kt
@@ -1,5 +1,6 @@
 package com.example.poulpejeu
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.hardware.Sensor
@@ -17,22 +18,30 @@ import android.view.animation.AnimationUtils
 import android.view.animation.TranslateAnimation
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.ComponentActivity
+import org.w3c.dom.Text
 
 class Soleil123 : ComponentActivity() {
     private lateinit var fille: ImageView
     private lateinit var runner: ImageView
+    private lateinit var tdm: ImageView
     private lateinit var sensorManager: SensorManager
     private lateinit var accelerometer: Sensor
     private lateinit var run : Button
+    private lateinit var count : TextView
+    private lateinit var score : TextView
     val translationAnim = TranslateAnimation(0f, 0f, 0f, -40f) // déplace de 200 pixels vers le haut
-    private val counterInterval = 1000L // intervalle de comptage en ms
+    private val counterInterval = 3000L // intervalle de comptage en ms
     private val countingPattern = arrayOf(10L, 10L, 10L, 10L) // temps de comptage et de pause en secondes
     private var currentCountIndex = -1 // index actuel dans le tableau de comptage
     private var isCounting = false // indique si le comptage est en cours
     private var counter = 0L
+    private var start:Long = 0
+    var end = false
 
 
+    @SuppressLint("MissingInflatedId", "ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
         val sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
         val accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
@@ -46,7 +55,11 @@ class Soleil123 : ComponentActivity() {
         runner = findViewById(R.id.runner)
         runner.setImageResource(R.drawable.runner)
 
+        count = findViewById(R.id.count)
         run = findViewById(R.id.Run)
+
+
+
 
         val animation = AnimationUtils.loadAnimation(this, R.anim.running_anim)
         translationAnim.duration = 10 // durée de l'animation en ms
@@ -54,6 +67,11 @@ class Soleil123 : ComponentActivity() {
 
         translationAnim.setAnimationListener(object : Animation.AnimationListener {
             override fun onAnimationStart(animation: Animation?) {
+                if (isCounting) {
+                    setContentView(R.layout.soleil_dead_layout)
+                    tdm = findViewById(R.id.tdm)
+                    tdm.setImageResource(R.drawable.tdm)
+                }
                 // Rien à faire ici
             }
 
@@ -69,7 +87,9 @@ class Soleil123 : ComponentActivity() {
         })
 
         run.setOnClickListener {
+            start = System.currentTimeMillis()
             runner.startAnimation(translationAnim)
+            startCounting()
         }
     }
     val sensorEventListener = object : SensorEventListener {
@@ -80,13 +100,16 @@ class Soleil123 : ComponentActivity() {
             val z = event.values[2]
 
             val acceleration = kotlin.math.sqrt(x * x + y * y + z * z)
-            Log.i("Vitesse",acceleration.toString())
+            //Log.i("Vitesse",acceleration.toString())
             if (acceleration > 15 && runner.translationY >= -1260) { // Choisissez un seuil approprié ici
                 runner.startAnimation(translationAnim)
                 Log.i("Position",runner.translationY.toString())
                 //val anim = AnimationUtils.loadAnimation(this, R.anim.running_anim)
                 //imageViewRunner.startAnimation(anim)
+            }else if(runner.translationY <= -1260 && !end){
+                victory()
             }
+
         }
 
         override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {
@@ -94,22 +117,57 @@ class Soleil123 : ComponentActivity() {
         }
     }
 
-    private fun startCountDown() {
-        object : CountDownTimer(countingPattern[currentCountIndex] * 1000L, 1000L) {
+    private fun startCounting() {
+        currentCountIndex = 0
+        startNextCounting()
+    }
+
+    private fun startNextCounting() {
+        if (currentCountIndex >= countingPattern.size) {
+            // Si on a atteint la fin du tableau, le joueur a perdu
+            Log.i("Fin","Fin du jeu")
+            setContentView(R.layout.soleil_dead_layout)
+            tdm = findViewById(R.id.tdm)
+            tdm.setImageResource(R.drawable.tdm)
+            return
+        }
+
+        val countDuration = countingPattern[currentCountIndex] * 1000L
+        val pauseDuration = 3000L
+        count.setText("Pas pause")
+
+        // Démarre le CountDownTimer pour le comptage
+        object : CountDownTimer(countDuration, counterInterval) {
             override fun onTick(millisUntilFinished: Long) {
-                // rien à faire ici
+                counter = millisUntilFinished / 1000L
+                Log.i("Temps", counter.toString())
             }
 
             override fun onFinish() {
-                currentCountIndex++
+                isCounting = !isCounting
+                count.setText("Pause")
+                // Démarre le CountDownTimer pour la pause
+                object : CountDownTimer(pauseDuration, counterInterval) {
+                    override fun onTick(millisUntilFinished: Long) {
+                        counter = millisUntilFinished / 1000L
+                    }
 
-                if (currentCountIndex < countingPattern.size) { // Si ce n'est pas le dernier temps de comptage
-                    startCountDown()
-                } else { // Si c'est le dernier temps de comptage
+                    override fun onFinish() {
+                        currentCountIndex++
 
-                }
+                        isCounting = !isCounting
+                        startNextCounting()
+                    }
+                }.start()
             }
-        }
+        }.start()
     }
+    fun victory(){
+        setContentView(R.layout.soleil_win_layout)
+        score = findViewById(R.id.score)
+        score.text = ((System.currentTimeMillis()-start)/1000.0).toString()+" secondes"
+        end = true
+    }
+
 
 }
