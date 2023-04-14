18aa69c0c3e6dbb882697fe7d445dee6b247fd17
t0ndre <thomas.andre@etudiant.univ-rennes1.fr>
2023-04-14 18:44:46 +0200

    correction musique

--- app/src/main/java/com/example/poulpejeu/MainActivity.kt
+++ app/src/main/java/com/example/poulpejeu/MainActivity.kt
@@ -46,61 +46,78 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
+    private lateinit var title: ImageView
+    private lateinit var bandeSon: MediaPlayer
+    private lateinit var buttonShout: Button
+    private lateinit var buttonQuizz: Button
+    private lateinit var buttonSoleil: Button
+    private lateinit var buttonBiscuit: Button
+    private lateinit var buttonBridge: Button
+    private lateinit var buttonRope: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.mainactivity_layout)
 
-        val title: ImageView = findViewById(R.id.poulpejeu)
+        title = findViewById(R.id.poulpejeu)
+        bandeSon = MediaPlayer.create(this,R.raw.bandeson)
+        buttonShout = findViewById(R.id.shoutButton)
+        buttonQuizz = findViewById(R.id.quizzButton)
+        buttonSoleil = findViewById(R.id.soleilButton)
+        buttonBiscuit = findViewById(R.id.biscuitButton)
+        buttonBridge = findViewById(R.id.bridgeButton)
+        buttonRope = findViewById(R.id.ropeButton)
+
         title.setImageResource(R.drawable.poulpejeu)
-        val bandeSon = MediaPlayer.create(this,R.raw.bandeson)
-        val buttonShout: Button = findViewById(R.id.shoutButton)
-        val buttonQuizz: Button = findViewById(R.id.quizzButton)
-        val buttonSoleil: Button = findViewById(R.id.soleilButton)
-        val buttonBiscuit: Button = findViewById(R.id.biscuitButton)
-        val buttonBridge: Button = findViewById(R.id.bridgeButton)
-        val buttonRope: Button = findViewById(R.id.ropeButton)
+
         bandeSon.setOnCompletionListener { bandeSon.start() }
 
         bandeSon.start()
-        // Ajoutez un écouteur de clic pour le bouton
+
+
         buttonShout.setOnClickListener {
             // Créez une Intent pour ouvrir votre nouvelle page
             val intent = Intent(this, ShoutGame::class.java)
-            bandeSon.stop()
             startActivity(intent)
         }
 
         buttonQuizz.setOnClickListener {
             // Créez une Intent pour ouvrir votre nouvelle page
             val intent = Intent(this, Quizz::class.java)
-            bandeSon.stop()
             startActivity(intent)
         }
 
         buttonSoleil.setOnClickListener {
             // Créez une Intent pour ouvrir votre nouvelle page
             val intent = Intent(this, Soleil123::class.java)
-            bandeSon.stop()
             startActivity(intent)
         }
         buttonBiscuit.setOnClickListener {
             // Créez une Intent pour ouvrir votre nouvelle page
             val intent = Intent(this, Biscuit::class.java)
-            bandeSon.stop()
             startActivity(intent)
         }
         buttonBridge.setOnClickListener {
             // Créez une Intent pour ouvrir votre nouvelle page
             val intent = Intent(this, BridgeGame::class.java)
-            bandeSon.stop()
             startActivity(intent)
         }
         buttonRope.setOnClickListener {
             // Créez une Intent pour ouvrir votre nouvelle page
             val intent = Intent(this, RopeGame::class.java)
-            bandeSon.stop()
             startActivity(intent)
         }
         requestRecordAudioPermission()
     }
+    override fun onPause() {
+        super.onPause()
+        bandeSon.pause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        bandeSon.start()
+    }
+
 }
