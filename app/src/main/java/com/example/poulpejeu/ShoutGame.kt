bbbfcdcabdbb57525656a4714c9f8a1e91c40e4f
t0ndre <thomas.andre@etudiant.univ-rennes1.fr>
2023-04-14 12:16:51 +0200

    ça commence les graphismes

--- app/src/main/java/com/example/poulpejeu/ShoutGame.kt
+++ app/src/main/java/com/example/poulpejeu/ShoutGame.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.media.MediaRecorder
 import android.os.Bundle
+import android.os.Handler
 import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.core.app.ActivityCompat
@@ -11,10 +12,12 @@ import androidx.core.content.ContextCompat
 import com.example.poulpejeu.R
 import java.lang.Math.log10
 import android.util.Log
+import android.view.View
+import android.widget.ProgressBar
 import android.widget.Toast
+import java.lang.Thread.sleep
 
 class ShoutGame : ComponentActivity(){
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.shoutgame_layout)
@@ -28,30 +31,41 @@ class ShoutGame : ComponentActivity(){
         mRecorder.prepare()
         mRecorder.start()
 
+
         val button: Button = findViewById(R.id.button)
         var volume: Double = 0.0;
 
+        val progressBar: ProgressBar = findViewById(R.id.progress_bar)
+        progressBar.max = 100
+        progressBar.progress = 0
+
         fun getAmplitude(): Double {
-            return if (mRecorder != null) {
-                20 * log10(mRecorder.maxAmplitude.toDouble());
-            } else {
-                0.0
-            }
+            return 20 * kotlin.math.log10(mRecorder.maxAmplitude.toDouble())
         }
 
         button.setOnClickListener {
-            runOnUiThread {
-                var temp: Double;
-                // Créez une Intent pour ouvrir votre nouvelle page
-                for (i in 1..100) {
-                    Thread.sleep(30)
-                    temp = getAmplitude()
-                    if (volume < temp){
-                        volume = temp
+
+            val handler = Handler()
+            handler.post(object : Runnable {
+                override fun run() {
+                    var temp: Double
+                    // Mettre à jour la barre de progression
+
+                    if (progressBar.progress < 100) {
+                        progressBar.progress += 1
+
+                        temp = getAmplitude()
+                        if (volume < temp) {
+                            volume = temp
+                        }
+                        handler.postDelayed(this, 30)
+                    }
+                    else {
+                        Toast.makeText(this@ShoutGame, volume.toString() + " dB", Toast.LENGTH_LONG).show()
+                        mRecorder.stop()
                     }
                 }
-                Toast.makeText(this, volume.toString() + " dB", Toast.LENGTH_LONG).show()
-            }
+            })
         }
     }
 }
