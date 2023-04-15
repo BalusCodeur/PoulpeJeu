ff094b502d20d5c551e71f339d61716a356c0b69
t0ndre <thomas.andre@etudiant.univ-rennes1.fr>
2023-04-15 20:17:51 +0200

    BridgeGame

--- app/src/main/java/com/example/poulpejeu/BridgeGame.kt
+++ app/src/main/java/com/example/poulpejeu/BridgeGame.kt
@@ -1,115 +1,115 @@
 package com.example.poulpejeu
 
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
+import android.view.ViewGroup
 import android.widget.Button
 import androidx.activity.ComponentActivity
 import android.widget.LinearLayout
 import android.widget.TextView
+import kotlin.math.roundToInt
 
 
-class BridgeGame : ComponentActivity(){
+class BridgeGame : ComponentActivity() {
     private var currentRow = 8
     private var dead = 0
-    private lateinit var data: Array<IntArray>
-    private lateinit var broke: BooleanArray
+    private lateinit var data: Array<BooleanArray>
     private lateinit var myTextView: TextView
-    private lateinit var buttonsLayout: LinearLayout
+    private lateinit var buttons: Array<Array<GlassButton>>
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.bridgegame_layout)
-        Log.v("log", "caca")
 
-        myTextView = findViewById<TextView>(R.id.dead)
+        myTextView = findViewById(R.id.dead)
         myTextView.setText("dead : " + dead)
 
+
+    var tab = IntArray(9)
+for (i in 0..8) {
+    tab[i] = Math.random().roundToInt()
+}
 // Tableau de données pour chaque ligne de boutons (1 = bouton correct, 0 = bouton incorrect)
         data = arrayOf(
-            intArrayOf(1, 0),
-            intArrayOf(0, 1),
-            intArrayOf(1, 0),
-            intArrayOf(0, 1),
-            intArrayOf(1, 0),
-            intArrayOf(0, 1),
-            intArrayOf(1, 0),
-            intArrayOf(0, 1),
-            intArrayOf(1, 0)
+            booleanArrayOf(tab[0]==0, tab[0]==1),
+            booleanArrayOf(tab[1]==0, tab[1]==1),
+            booleanArrayOf(tab[2]==0, tab[2]==1),
+            booleanArrayOf(tab[3]==0, tab[3]==1),
+            booleanArrayOf(tab[4]==0, tab[4]==1),
+            booleanArrayOf(tab[5]==0, tab[5]==1),
+            booleanArrayOf(tab[6]==0, tab[6]==1),
+            booleanArrayOf(tab[7]==0, tab[7]==1),
+            booleanArrayOf(tab[8]==0, tab[8]==1),
+
+            )
+
+        for (i in 0..8)
+
+        buttons = arrayOf<Array<GlassButton>>(
+            arrayOf(findViewById(R.id.button0_0), findViewById(R.id.button0_1)),
+            arrayOf(findViewById(R.id.button1_0), findViewById(R.id.button1_1)),
+            arrayOf(findViewById(R.id.button2_0), findViewById(R.id.button2_1)),
+            arrayOf(findViewById(R.id.button3_0), findViewById(R.id.button3_1)),
+            arrayOf(findViewById(R.id.button4_0), findViewById(R.id.button4_1)),
+            arrayOf(findViewById(R.id.button5_0), findViewById(R.id.button5_1)),
+            arrayOf(findViewById(R.id.button6_0), findViewById(R.id.button6_1)),
+            arrayOf(findViewById(R.id.button7_0), findViewById(R.id.button7_1)),
+            arrayOf(findViewById(R.id.button8_0), findViewById(R.id.button8_1))
         )
 
-        broke = BooleanArray(9){false}
-// Créez une vue LinearLayout pour contenir les boutons
-        buttonsLayout = findViewById<LinearLayout>(R.id.buttons)
-        buttonsLayout.orientation = LinearLayout.VERTICAL
 
-// Boucle pour créer chaque ligne de boutons
-        for (i in data.indices) {
-            val row = LinearLayout(this)
-            row.orientation = LinearLayout.HORIZONTAL
 
-            // Boucle pour créer chaque bouton dans la ligne
+        for (i in buttons.indices) {
             for (j in 0..1) {
-                val button = Button(this)
-                button.layoutParams = LinearLayout.LayoutParams(
-                    LinearLayout.LayoutParams.WRAP_CONTENT,
-                    LinearLayout.LayoutParams.WRAP_CONTENT,
-                    1f
-                ) // Les boutons s'étendent également pour remplir l'espace disponible
-                button.text = if (data[i][j] == 1) "Correct" else "Incorrect"
-                if(i < 8){
-                    button.isEnabled = false
+                buttons[i][j].init(data[i][j])
+                if (i == 8) {
+                    buttons[i][j].active()
                 }
-                button.setOnClickListener{
-                    onClick(i,j,button)
+                buttons[i][j].setOnClickListener {
+                    onClick(i, j)
                 }
-                row.addView(button)
             }
 
-            // Ajoutez la ligne de boutons à la vue LinearLayout
-            buttonsLayout.addView(row)
         }
     }
 
-    private fun onClick(i:Int, j:Int, button:Button){
-        if ( i == 0 && data[i][j] == 1){
-            finish()
-        }
-        else {
-            if (data[i][j] == 1) {
-                val previousRow = buttonsLayout.getChildAt(i - 1) as LinearLayout
-                if(i == currentRow) {
-                    if(broke[i-1]){
-                        if(data[i-1][0] == 1){
-                            previousRow.getChildAt(0).isEnabled = true
-                        }
-                        else{
-                            previousRow.getChildAt(1).isEnabled = true
-                        }
-                    }
-                        else {
-                            previousRow.getChildAt(0).isEnabled = true
-                            previousRow.getChildAt(1).isEnabled = true
-                    }
-                    currentRow--;
+
+    private fun onClick(i: Int, j: Int) {
+        if (i == currentRow) {
+
+            if (buttons[i][j].discover()) {
+
+                if (currentRow == 0) {
+                    finish()
+                } else {
+
+                    buttons[i - 1][0].active()
+                    buttons[i - 1][1].active()
+
+                    currentRow--
                 }
+
             } else {
                 dead++;
                 myTextView.setText("dead : " + dead)
-                button.isEnabled = false
-                broke[i]=true;
                 reInit()
             }
         }
+
     }
 
-    private fun reInit(){
+
+    private fun reInit() {
         currentRow = 8;
-        for (i in data.indices) {
-            if (i < 8){
-                val cursor = buttonsLayout.getChildAt(i) as LinearLayout
-                cursor.getChildAt(0).isEnabled = false
-                cursor.getChildAt(1).isEnabled = false
+        for (i in buttons.indices) {
+            if (i < 8) {
+                buttons[i][0].reInit()
+                buttons[i][1].reInit()
             }
         }
     }
---
+++ app/src/main/java/com/example/poulpejeu/GlassButton.kt
@@ -0,0 +1,53 @@
+package com.example.poulpejeu
+
+import android.content.Context
+import android.graphics.Color
+import android.util.AttributeSet
+import android.widget.LinearLayout
+import androidx.appcompat.widget.AppCompatButton
+
+class GlassButton @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : AppCompatButton(context, attrs, defStyleAttr) {
+    private var isBroken = false
+    private var weak = false
+
+    private fun breakGlass() {
+        isBroken = true
+        isActivated = false
+        this.setBackgroundColor(Color.RED);
+    }
+
+    fun init(weak: Boolean) {
+
+        this.weak = weak
+        isActivated = false
+        this.setBackgroundColor(Color.GRAY);
+
+    }
+
+    fun reInit() {
+        if (!isBroken) {
+            isActivated = false
+            this.setBackgroundColor(Color.GRAY);
+        }
+    }
+
+    fun active() {
+        if (!isBroken) {
+            isActivated = true
+            this.setBackgroundColor(Color.BLUE);
+        }
+    }
+
+    fun discover(): Boolean {
+        return if (weak) {
+            breakGlass()
+            false
+        } else {
+            active()
+            true
+        }
+    }
+
+}
